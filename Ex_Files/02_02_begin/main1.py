e183b2d8f173eadd0fa157f35822e0a4f5d1abaf
fly-2high <fares.per@gmail.com>
2023-06-04 00:25:08 +0000

    Add

--- Ex_Files/02_02_begin/main1.py
+++ Ex_Files/02_02_begin/main1.py
@@ -4,4 +4,4 @@ extended_greet="hello world," + " this is a long string"
 
 extended2_greet=greet + "this is a long string"
 
-print(extended_greet,extended2_greet)
+print(extended_greet,extended2_greet) 
