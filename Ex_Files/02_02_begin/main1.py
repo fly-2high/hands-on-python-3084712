6da6c44fc129e304fd4c8cfaec5bec88204f1e9d
fly-2high <fares.per@gmail.com>
2023-06-04 01:26:43 +0000

    Change

--- Ex_Files/02_02_begin/main1.py
+++ Ex_Files/02_02_begin/main1.py
@@ -5,3 +5,16 @@ extended_greet="hello world," + " this is a long string"
 extended2_greet=greet + "this is a long string"
 
 print(extended_greet,extended2_greet) 
+
+name="john"
+
+intrupution= f"Hello {name}" 
+
+greet_format="hello {}"
+
+formatted=greet_format.format(name)
+
+print(intrupution,formatted)
+print(formatted.upper())
+print(formatted.lower())
+print(formatted.replace("john","paul"))
