ad881c1f9fe2038ab4f3c42c1a05b6f24093af39
fly-2high <fares.per@gmail.com>
2023-06-12 02:37:33 +0000

    Change

--- Ex_Files/04_02_begin/main.py
+++ Ex_Files/04_02_begin/main.py
@@ -1,3 +1,27 @@
-from flask import Flask
+import csv
+from distutils.log import debug
+from flask import Flask,render_template,jsonify
 
 app = Flask(__name__)
+
+with open("laureates.csv", "r", encoding="utf-8") as f:
+    reader=csv.DictReader(f)
+    laureates=list(reader)
+
+@app.route("/")
+#def hello():
+#    return "Hello, World"
+
+#app.run(debug=True)
+def index():
+    # template found in templates/index.html
+    return render_template("index.html")
+
+
+@app.route("/laureates/")
+def laureate():
+    return jsonify(laureates)
+
+app.run(debug=True)
+
+
